396aa875d50283b7d2854b6b442e8831f44ce6b3
elhult <ludvig.hult@uu.se>
2024-12-17 13:55:20 +0000

    Reduce debugging noise

--- test_pymc.py
+++ test_pymc.py
@@ -1,7 +1,5 @@
 print(f"Running test file {__file__}")
 import pymc as pm
-import pytensor
-print(pytensor.config)
 with pm.Model() as model:
     x = pm.Normal("x", mu=0, sigma=1)
     trace = pm.sample()
