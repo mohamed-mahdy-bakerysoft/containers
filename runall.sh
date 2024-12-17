940a59db0a3ee498eeeca70042064feff8328193
elhult <ludvig.hult@uu.se>
2024-12-17 09:43:27 +0000

    fixing some mistakes
    1) dont name the python files the same as the modules that import them
    2) copy the tests into the container
    3) run apt-get update, since it otherwise cannot locate the mesa and xvfb packages

--- runall.sh
+++ runall.sh
@@ -1,13 +1,11 @@
-# Run the script with sudo!
-
-singularity build --force base.sif base.def
+sudo singularity build --force --notest base.sif base.def
 singularity test      base.sif
 singularity test --nv base.sif
 
-singularity build --force pip.sif pip.def
+sudo singularity build --force --notest pip.sif pip.def
 singularity test      pip.sif
 singularity test --nv pip.sif
 
-singularity build --force pyvista.sif pyvista.def
-singularity test      pip.sif
-singularity test --nv pip.sif
+sudo singularity build --force --notest pyvista.sif pyvista.def
+singularity test      pyvista.sif
+singularity test --nv pyvista.sif
--- test_pyvista.py
+++ test_pyvista.py
@@ -2,4 +2,4 @@ import pyvista as pv
 pv.start_xvfb()
 pl=pv.Plotter(off_screen=True)
 pl.add_mesh(pv.Sphere())
-pl.show(screenshot='f.png')
+pl.show(screenshot='sphere.png')
